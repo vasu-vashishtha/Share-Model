52d7bb919b9a44cc58c0bf08e285acace3dcb54e
vasu-vashishtha <vashishthavasu07@gmail.com>
2023-03-22 15:34:01 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+
+
+//Modal open function
+function openModal() {
+  console.log('Modal is open');
+  modal.classList.add("active");
+  overlay.classList.add("overlayactive");
+};
+
+//Modal close function
+function closeModal() {
+  modal.classList.remove("active");
+  overlay.classList.remove('overlayactive');
+};
+
+
+
+
